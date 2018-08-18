68f7199a342df4aa6c863f803b775814fe22db5c
Fox4Raize <shiamatsu@gmail.com>
2018-08-18 14:27:50 -0700

    base in-game UI

---
+++ Assets/NoteEngine.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteEngine : MonoBehaviour {
+
+    bool active = false;
+    GameObject note;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        active = true;
+        Debug.Log(collision.gameObject.tag);
+        note = collision.gameObject;
+    }
+
+    public void DestroyNote()
+    {
+        Destroy(note);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        active = false;
+    }
+}
