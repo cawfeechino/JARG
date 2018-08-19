0b5653a54b275de8927b2ee1b82ed642c5bb06e2
Fox4Raize <shiamatsu@gmail.com>
2018-08-18 17:19:08 -0700

    some progress of in-game UI

---
+++ Assets/CreationTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreationTest : MonoBehaviour {
+
+    public Rigidbody2D note;
+    public Transform location;
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Instantiate(note, location.position, location.rotation);
+	}
+}
